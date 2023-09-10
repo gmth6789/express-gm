6fa4102e4099fb56b2297d219af5115691bd21e1
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-09-10 19:36:07 +0700

    2 files modified

--- app.js
+++ app.js
@@ -6,16 +6,35 @@ const path = require('path');
 const admin = require("firebase-admin");
 const credentials = require("./serviceAccountKey.json");
 
+admin.initializeApp({
+  credential: admin.credential.cert(credentials)
+});
+
+app.post('/signup', async (req, res) => {
+  console.log(req);
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  }
+  const userResponse = await admin.auth().createUser({
+    email: user.email,
+    password: user.password,
+    emailVerified: false,
+    disabled: false
+  });
+  res.json(userResponse);
+})
+
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use(express.static('static'));
+// app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('pages/index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve('pages/index.html'));
+// });
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
