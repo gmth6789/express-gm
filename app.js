90668db30f0b03293ecb3569e3924a098c295ced
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-09-10 19:23:17 +0700

    1 file modified, 1 added, 1 deleted

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 const path = require('path');
 
+const admin = require("firebase-admin");
+const credentials = require("./serviceAccountKey.json");
+
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-
-
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
